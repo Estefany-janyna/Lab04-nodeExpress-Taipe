65f974388b600ce4ecbe11508b6e9e77b0e0e6a3
Estefany-janyna <estefany.taipe@tecsup.edu.pe>
2023-11-12 09:11:19 -0500

    first commit

--- app.js
+++ app.js
@@ -21,6 +21,11 @@ app.get('/productos', (req, res) => {
     res.sendFile(`${__dirname}/views/productos.html`)
 });
 
+// Ruta para mostrar la lista de contactos
+app.get('/contactos', (req, res) => {
+  res.sendFile(`${__dirname}/views/contactos.html`)
+});
+
 app.listen(port, () => {
   console.log(`Servidor Express escuchando en el puerto ${port} en http://localhost:5000`);
 });
